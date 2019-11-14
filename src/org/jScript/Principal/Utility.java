0740f7ef121e27d7fc72230e4db4acd06f287b8b
Luis Gerardo Collazos Castro <inge.gerardoc@gmail.com>
2019-11-14 13:53:15 -0500

    Comando Write-Host

--- src/org/jScript/Principal/Utility.java
+++ src/org/jScript/Principal/Utility.java
@@ -9,10 +9,30 @@ public class Utility {
 				getHost(cmlet);				
 			} else if (comando[0].equalsIgnoreCase("Get-Command") && cmlet.length() >= 11 ) {
 				getCommand(cmlet);
+			} else if(comando[0].equalsIgnoreCase("Write-Host") && cmlet.length() >= 10 ) {
+				writeHost(cmlet);
 			} else {
 				System.out.print("");
 			}
 		}
+	}
+
+	private void writeHost(String cmlet) {
+		// TODO Auto-generated method stub
+		if(cmlet.length() > 11) {
+			String[] comando = cmlet.split(" ");
+			if(comando.length > 0) {
+				String str = cmlet.substring(11);
+				if(str.charAt(0) == '"' && str.charAt(str.length()-1) == '"') {
+					System.out.println(str.substring(1, str.length()-1));
+				}else {
+					System.out.println(str);
+				}				
+			}
+		} else {
+			System.out.print("");
+		}
+		
 	}
 
 	private void getCommand(String cmlet) {
@@ -22,13 +42,12 @@ public class Utility {
 		System.out.println(" Exit          1.0");
 		System.out.println(" Get-Host      1.0");
 		System.out.println(" Get-Command   1.0");
+		System.out.println(" Write-Host    1.0");
 	}
 
 	private void getHost(String cmlet) {
 		String version = "Version 1.0 --> 2019/11/13";
 		System.out.println("   " + version);
 	}
-	
-	
 
 }
